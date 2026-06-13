6ce3e50da220a8557002a9748735379a481b4133
Rodriguesmath <rodriguesmateus.rm@gmail.com>
2026-06-13 10:50:49 -0300

    feat: add LevelSensor_Reset function declaration to LevelSensor.h

--- Core/Inc/LevelSensor.h
+++ Core/Inc/LevelSensor.h
@@ -14,7 +14,10 @@
 void LevelSensor_NewSample(int raw);
 void LevelSensor_IsReady(void);
 uint16_t LevelSensor_GetAverage(void);
+void LevelSensor_Reset(void);
+
 uint16_t LevelSensor_RawToMilliVolts(uint16_t raw);
 uint8_t LevelSensor_RawToPercent(uint16_t raw);
+
 
 #endif /* INC_LEVELSENSOR_H_ */
