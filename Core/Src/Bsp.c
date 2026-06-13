83ac6d0f94e9a7896596ed010a150e21d644dc7b
Rodriguesmath <rodriguesmateus.rm@gmail.com>
2026-06-13 10:29:28 -0300

    feat: implement timer functionality in Bsp.c with initialization and elapsed time tracking

--- Core/Src/Bsp.c
+++ Core/Src/Bsp.c
@@ -6,14 +6,41 @@
  */
 #include "Bsp.h"
 
-void Bsp_Init(void){}
+extern ADC_HandleTypeDef hadc1;
+extern TIM_HandleTypeDef htim6;
 
-int Bsp_TimerHasElapsed(void){}
+static volatile int timer6_elapsed = 0;
 
-void Bsp_StartTimerInterrupt(void){}
+void Bsp_Init(void)
+{
+    timer6_elapsed = 0;
+}
 
-void Bsp_ClearTimerFlag(void){}
+int Bsp_TimerHasElapsed(void)
+{
+    return timer6_elapsed;
+}
+
+void Bsp_StartTimerInterrupt(void)
+{
+    HAL_TIM_Base_Start_IT(&htim6);
+}
+void Bsp_ClearTimerFlag(void)
+{
+    timer6_elapsed = 0;
+}
+
 
 int Bsp_ReadAdcPolling(void){}
 
-void Bsp_UartPrintAverage(uint16_t average){}
+void Bsp_UartPrintAverage(uint16_t average)
+{
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM6)
+    {
+        timer6_elapsed = 1;
+    }
+}
