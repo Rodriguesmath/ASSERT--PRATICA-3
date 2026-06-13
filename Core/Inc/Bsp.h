293e5bfbba0a1fee3ff68d70e1c9290ebde5928e
Rodriguesmath <rodriguesmateus.rm@gmail.com>
2026-06-13 09:48:36 -0300

    feat: initial source and header files for Bsp and LevelSensor modules

---
+++ Core/Inc/Bsp.h
@@ -0,0 +1,13 @@
+/*
+ * bsp.h
+ *
+ *  Created on: 13 de jun. de 2026
+ *      Author: Rodrigues
+ */
+
+#ifndef INC_BSP_H_
+#define INC_BSP_H_
+
+
+
+#endif /* INC_BSP_H_ */
---
+++ Core/Src/LevelSensor.C
@@ -0,0 +1,8 @@
+/*
+ * LevelSensor.C
+ *
+ *  Created on: 13 de jun. de 2026
+ *      Author: Rodrigues
+ */
+
+
